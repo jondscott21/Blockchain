24cf941c8cb2c1588f2ed7debb751ce73f6c61df
Jonathan Scott <jonathan.scott@gmail.com>
2020-01-16 16:16:29 -0800

    added a refresh

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import Transaction from './Transaction'
 
 function App() {
-  // const [mining, setMining] = useState(false)
+  // const [update, setMining] = useState(false)
   const [login, setLogin] = useState(false)
   const [username, setUsername] = useState('')
   const [chain, setChain] = useState([])
@@ -19,12 +19,21 @@ function App() {
     e.preventDefault()
     setLogin(true)
   }
-  // const handleMineClick = () => {
-  //   setMining(true)
-  //   axios.get(`http://localhost:5000/last_block`)
-  //   .then(res => console.log(res.data.last_block))
-  //   .catch(err => console.log(err.response))
-  // }
+  const refresh = () => {
+    setBalance(0)
+    if(chain.length > 1 && login === true) {
+      let arr = []
+      arr = [...chain.map(el => el.transactions)]
+      let flattened = [].concat(...arr)
+      let userArray = []
+      flattened.forEach(el => {
+        if(el.sender === username || el.recipient.slice(0, -1) === username) userArray.push(el)
+        if(el.sender === username) setBalance(b => b -= Number(el.amount))
+        if(el.recipient.slice(0, -1) === username) setBalance(b => b += Number(el.amount))
+      })
+      setTransactions(userArray)
+    }
+  }
   const handleLogOut = () => {
     setLogin(false)
   }
@@ -38,6 +47,7 @@ function App() {
     }
   }, [login])
   useEffect(() => {
+    setBalance(0)
     if(chain.length > 1 && login === true) {
       let arr = []
       arr = [...chain.map(el => el.transactions)]
@@ -59,7 +69,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 style={{justifySelf: 'flex-start'}}>Your Lambda Wallet</h3>
-        {login && <button onClick={handleLogOut}>Log Out</button>}
+        {login && <>
+          <button onClick={refresh}>Refresh</button>
+          <button onClick={handleLogOut}>Log Out</button>
+        </>}
       </header>
       {login && <>
         <h3>{username}</h3>
